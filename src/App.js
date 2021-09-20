9efb1bb560fc0a31323694dadcf3ef88dbca93fa
Leonardo <leonardo.appio@gmail.com>
2021-09-20 20:26:17 -0300

    criando o componente PostCard

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
 import './App.css';
 import { Component } from 'react';
+import { PostCard } from './components/PostCard';
+import { loadPosts } from './utils/load-posts'
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    }
+  state = {
+    posts: []
   };
-
-
-  // life cycle methods
-  componentDidMount() {
-    this.loadPosts();
+  // life cycle method
+  async componentDidMount() {
+    await this.loadPosts();
 
   }
-
-  componentDidUpdate() {
-  }
-
-  componentWillUnmount() {
-  }
-  // fim dos life cycle methods
-
 
   loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url}
-    });
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos })
 
   }
@@ -47,13 +26,10 @@ class App extends Component {
       <section className="container">
         <div className="posts">
           {posts.map(post => (
-            <div className="post">
-              <img src = {post.cover} alt = {post.title}/>
-              <div key={post.id} className="post-content">
-                <h1> {post.title} </h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
+            <PostCard
+              key={post.id}
+              post={post}
+            />
           ))}
         </div>
       </section>
