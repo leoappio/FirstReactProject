70125e666f9ff7a1b5a7727550452602961fa596
Leonardo <leonardo.appio@gmail.com>
2021-09-20 20:05:43 -0300

    preenchendo dados com api

--- src/App.js
+++ src/App.js
@@ -6,43 +6,57 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [
-        {
-          id: 1,
-          title: 'Titulo Objeto 1',
-          body: 'Corpo Objeto 1'
-        },
-        {
-          id: 2,
-          title: 'Titulo Objeto 2',
-          body: 'Corpo Objeto 2'
-        },
-        {
-          id: 3,
-          title: 'Titulo Objeto 3',
-          body: 'Corpo Objeto 3'
-        }
-      ]
+      posts: []
     }
+  };
+
+
+  // life cycle methods
+  componentDidMount() {
+    this.loadPosts();
+
   }
 
-  clickName = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 })
+  componentDidUpdate() {
+  }
+
+  componentWillUnmount() {
+  }
+  // fim dos life cycle methods
+
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url}
+    });
+    this.setState({ posts: postsAndPhotos })
+
   }
 
   render() {
     const { posts } = this.state;
     return (
-      <div className="App">
-        {posts.map(post => (
-          <div key={post.id}>
-            <h1> {post.title} </h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post">
+              <img src = {post.cover} alt = {post.title}/>
+              <div key={post.id} className="post-content">
+                <h1> {post.title} </h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
